61cc812328400623531ad755f95ca8abb6416e23
yuvraj manale <yuvrajmnale@gmail.com>
2026-06-24 19:18:13 +0530

    fixed Home

--- dashboard/src/Components/Home.jsx
+++ dashboard/src/Components/Home.jsx
@@ -1,34 +1,4 @@
-// import React, { useEffect } from 'react';
-// import Dashboard from './Dashboard';
-// import TopBar from './TopBar';
 
-// const Home = () => {
-
-//   useEffect(() => {
-//     const params = new URLSearchParams(window.location.search);
-//     const token = params.get("token");
-
-//     if (token) {
-//       localStorage.setItem("token", token);
-//       window.history.replaceState({}, document.title, "/#/");
-//     }
-
-//     const storedToken = localStorage.getItem("token");
-
-//     if (!storedToken) {
-//       window.location.href = "https://stock-trading-platform-l4d8-rj31o9tii-yuvraj-manales-projects.vercel.app";
-//     }
-//   }, []);
-
-//   return (
-//     <>
-//       <TopBar />
-//       <Dashboard />
-//     </>
-//   );
-// };
-
-// export default Home;
 
 import React, { useEffect } from "react";
 import Dashboard from "./Dashboard";
